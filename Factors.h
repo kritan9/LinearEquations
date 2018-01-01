1eab59282d46fdd07e93b33102c77aa9efd20103
kritan <kritanbanstola1212@gmail.com>
2018-01-02 02:34:05 +0700

    minor changes

--- Factors.h
+++ Factors.h
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
-float HCF(float a,float b){
-	float c;
+double HCF(double a,double b){
+	double c;
 	while(a!=0){
 		c=(int)b%((int)a);
 		b=a;
-		a=(float)c;
+		a=(double)c;
 	}
 	return b;
 }
 
-float LCM(float a,float b){
-	float d=a,e=b;
+double LCM(double a,double b){
+	double d=a,e=b;
 	int c;
 	while(a!=0){
 		c=(int)b%((int)a);
 		b=a;
-		a=(float)c;
+		a=(double)c;
 	}
 	return d*e/b;
 }
--- LinearEquationsBeta.c
+++ LinearEquationsBeta.c
@@ -2,18 +2,20 @@
 #include <time.h>
 #include <stdlib.h>
 #include <math.h>
-
+#include "Factors.h"
+#include "properfraction.h"
 void swap(double *a,double *b);
-void fraction(double a,int *x,int *y);
+
 
 int main(){
 
    int n=10;
    int i,j,k;
-   double l;
+   double l,m;
    double z;
    int r,t;
    int o=0;int p;
+   double q=1;
    printf("This program solves linear equations of n variables\n How to use?\n-Eg, if you want to solve for 2 variables, say x+y=2 and x-y=0, Enter number of variables 2 then input coefficients in the format\n1 1 2\n1 -1 0\n");
 
   A:
@@ -41,23 +43,13 @@ while(i<(n-1)){
     k=1;p=1;
     while(k<(n-i)){
 
-            if(var[i][i]==0){
-                if(p==(n-i)){
-                printf("Infinite or Undefined Solution \n");
-                goto A;
-                }
-                for(o=0;o<=n;o++){
-
-                    swap(&var[i][o],&var[i+p][o]);
-
-                }
-                p++; continue;
-            }
-          l=var[i+k][i]/var[i][i];
+             l=LCM(var[i+k][i],var[i][i]);
+          l=l/var[i][i];
+          m=l*var[i][i]/var[i+k][i];
             j=i;
         while(j<(n+1)){
 
-                var[i+k][j] = var[i+k][j]-(var[i][j]*l);
+                var[i+k][j] = var[i+k][j]*m-(var[i][j]*l);
                 j++;
               }
               k++;
@@ -81,67 +73,6 @@ for(i=n-1;i>=0;i--){
     x[i]=l/var[i][i];
 
 }
-for(i=0;i<n;i++){
-
-    fraction(x[i],&ans[i][0],&ans[i][1]);
-   //printf(" %d %d ",ans[i][0],ans[i][1]);
-    r=(int)(((double)ans[i][0]/ans[i][1])*1000000);
-    t=(int)(x[i]*1000000);
-    printf("\n x%d = ",i+1);
-    if(((t-r)<=1) && ((r-t)<=1)){
-    if(ans[i][1]==1)
-    printf("%d = ",ans[i][0]);
-else
-     printf("%d/%d = ",ans[i][0],ans[i][1]);
-    }
-    printf(" %lf ",x[i]);
 }
 
-    getch();
-    return 0;
-}
-void fraction(double a,int *x,int *y){
-    int m=1,n=1;
-    int k,z,p;
-   p=z=(int)(a*1000000);
-   if(z<0) { z=z*(-1);}
-   if(z!=0){
-    for(m=1;m<=100000;m++){
-        for(n=1;n<=100000;n++){
-            k=(int)(1000000*(double)m/n);
-			if((k+1)<z) break;
-            if(((k-z)<=1) && ((z-k)<=1) ){
-                if (z==p){
-                *x=m;
-                *y=n;}
-                else {
-                    *x=m*(-1);
-                    *y=n;
-                }
-                break;
-            }
 
-        }
-         if(((k-z)<=1) && ((z-k)<=1)) break;
-   }
-   if(m==1001 && n==1001){
-        *x=1;*y=1;
-   }
-   }
-
-
-   if(z==0){
-
-    *x=0;
-    *y=1;
-   }
-
-
-}
-void swap(double *a,double *b){
-    double c,d;
-    c=*b;
-    d=*a;
-    *a=c;*b=d;
-
-}
